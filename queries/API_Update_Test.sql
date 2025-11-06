c1b8033fc5585c73f6c1d7de6d6cc192306af2c2
saipythondeveloper <hemanthsai.sai109@gmail.com>
2025-11-06 14:01:48 -0600

    Add SQL query: API Update Test

--- queries/API_Update_Test.sql
+++ queries/API_Update_Test.sql
@@ -1,1 +1,1 @@
-select * from luma_loyalty_dataset_navdeep where name='Hemanth' and date='2025-10-31' and city='texas'
+select * from luma_loyalty_dataset_navdeep where name='Hemanth' and date='2025-10-31' and city='texas' and country='us'
